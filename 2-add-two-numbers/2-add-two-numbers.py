a69608183891c9bb11f3580332540d59f3276bc8
Hazem Hany <hazemhany09@gmail.com>
2022-07-01 22:25:47 +0200

    Time: 74 ms (86.23%), Space: 14 MB (10.25%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -7,8 +7,7 @@ class ListNode:
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         newNode = ListNode((l1.val + l2.val) % 10)
-        carry = 0
-        if (l1.val + l2.val) > 9: carry = 1;
+        carry = (l1.val + l2.val)//10
         x = newNode
         l1 = l1.next
         l2 = l2.next
@@ -16,10 +15,7 @@ class Solution:
             newNode.next = ListNode()
             newNode =  newNode.next
             newNode.val = (carry + l1.val + l2.val) % 10
-            if carry + l1.val + l2.val > 9:
-                carry = 1
-            else:
-                carry = 0
+            carry = (carry + l1.val + l2.val)//10
             l1 = l1.next
             l2 = l2.next
             
@@ -27,20 +23,13 @@ class Solution:
             newNode.next = ListNode()
             newNode =  newNode.next
             newNode.val = (carry + l2.val) % 10
-            if carry + l2.val > 9:
-                carry = 1
-            else:
-                carry = 0
-
+            carry = (carry +  l2.val)//10
             l2 = l2.next
         while(l1 != None):
             newNode.next = ListNode()
             newNode =  newNode.next
             newNode.val = (carry + l1.val) % 10
-            if carry + l1.val > 9:
-                carry = 1
-            else:
-                carry = 0
+            carry = (carry + l1.val )//10
             l1 = l1.next
         if carry:
             newNode.next = ListNode()

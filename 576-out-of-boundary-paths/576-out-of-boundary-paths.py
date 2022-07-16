c33ad9f6b92d4e605cc13562e1d93b8f88afc57a
Hazem Hany <hazemhany09@gmail.com>
2022-07-16 16:07:17 +0200

    Time: 185 ms (62.15%), Space: 16.6 MB (70.33%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -7,15 +7,16 @@ class Solution:
             return 1
         if(moves == 0):
             return 0
-        if(self.lookup[r][c][moves-1]!=-1):
-            return self.lookup[r][c][moves-1]
+        key = f"{r}-{c}-{moves}"
+        if(key in self.lookup):
+            return self.lookup[key]
         answer = self.rec(r-1,c,moves-1) + self.rec(r,c-1,moves-1) + self.rec(r+1,c,moves-1) + self.rec(r,c+1,moves-1)
-        self.lookup[r][c][moves-1] = answer
+        self.lookup[key] = answer
         return answer
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         self.m = m
         self.n = n
-        self.lookup = [[[-1 for k in range(maxMove)] for j in range(n)] for i in range(m)]
+        self.lookup = {}
         r = self.rec(startRow,startColumn,maxMove)
         # print(self.lookup)
         # return r 

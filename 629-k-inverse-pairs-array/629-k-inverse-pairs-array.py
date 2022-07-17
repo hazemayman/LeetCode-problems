c41c1198ae852d02caa511e4c1d9f6986f473f2e
Hazem Hany <hazemhany09@gmail.com>
2022-07-17 18:35:59 +0200

    Time: 1295 ms (21.25%), Space: 53 MB (40.00%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
@@ -5,15 +5,13 @@ class Solution:
         lookup[0] = [0] * (k+1)
         m = 1000000007
         for i in range(1 , n+1):
-            for j in range(0 , k+1):
-                if(i == 1 and j == 0):
-                    lookup[i][j] = 1 
-                    break
-                elif(j == 0):
+            j = 0
+            while(j <= k and j <= i * (i - 1) / 2):
+                if(j == 0):
                     lookup[i][j] = 1
                 else:
                     val = lookup[i-1][j - i]  if j - i >= 0 else 0
-                    val =  (lookup[i-1][j] - val)
-                    lookup[i][j] = (lookup[i][j-1] +  val )%m                                                                                                           
+                    lookup[i][j] = (lookup[i][j-1] +  lookup[i-1][j] -val )%m
+                j+=1
                         
         return lookup[n][k]

eadd6efdb69c19b8f963f5b12cfc990744fdabf4
Hazem Hany <hazemhany09@gmail.com>
2022-07-19 11:55:30 +0200

    Time: 31 ms (93.47%), Space: 13.8 MB (66.48%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -5,11 +5,8 @@ class Solution:
             triangle.append([1,1])
             
         for i in range(1 , numRows-1):
-            a = [1] * (i+2)
-            k = 1
-            while(k<=i):
-                a[k] = sum(triangle[i][k-1:k+1])
-                k+=1
+            a = [1,1]
+            a[1:1] = [sum(triangle[i][k-1:k+1]) for k in range(1 , i+1)]
             triangle.append(a)
         return triangle
             

7c07a0945262ec65afc5f13ebef37c512af6eb09
Hazem Hany <hazemhany09@gmail.com>
2022-07-19 12:25:21 +0200

    Time: 84 ms (26.48%), Space: 13.8 MB (96.32%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 : return 0
+        if x == 1 : return 1
+        minimum = 1
+        maximum = x//2
+        
+        while(int(maximum) != int(minimum)):
+            middle = (maximum + minimum) / 2
+            result = middle * middle
+            if(result == x):
+                return int(middle)
+            if(result > x):
+                maximum = middle
+            else:
+                minimum = middle
+        return int(maximum)
+        
+        

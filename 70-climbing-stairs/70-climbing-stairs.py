067dbdba5871bcb933f0fee34ed147761de359a4
Hazem Hany <hazemhany09@gmail.com>
2022-07-21 16:20:49 +0200

    Time: 54 ms (27.23%), Space: 13.8 MB (56.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,12 +1,7 @@
 class Solution:
     lookup = {}
     def climbStairs(self, n: int) -> int:
-        if(n == 1):
-            return 1
-        if(n == 2):
-            return 2
-        if(n in self.lookup):
-            return self.lookup[n]
-        value = self.climbStairs(n-1) + self.climbStairs(n-2)
-        self.lookup[n] = value
-        return value
+        lookup = [1 , 2]
+        for i in range(2 , n):
+            lookup.append(lookup[i-1] + lookup[i-2])
+        return lookup[n-1]

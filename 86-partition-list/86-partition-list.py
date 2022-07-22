291e1c2cf8696f6d1401597c5b7257ead711321c
Hazem Hany <hazemhany09@gmail.com>
2022-07-22 11:22:07 +0200

    Time: 41 ms (82.91%), Space: 13.9 MB (75.71%) - LeetHub

--- 86-partition-list/86-partition-list.py
+++ 86-partition-list/86-partition-list.py
@@ -5,48 +5,28 @@ class ListNode:
         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        startingHead = head
-        before_x_greater = ListNode()
-        p1 = before_x_greater
         before_x_smaller = ListNode()
-        p2 = before_x_smaller
+        p1 = before_x_smaller
         fromX = ListNode()
-        p3 = fromX
+        p2 = fromX
         
         condition = False
         while(head != None):
             if(head.val < x):
                 a = ListNode(head.val)
-                p2.next = a
-                p2 = p2.next
-            elif(head.val > x and not condition):
-                a = ListNode(head.val)
                 p1.next = a
                 p1 = p1.next
-            elif(head.val >= x and condition):
+            elif(head.val >= x):
                 a = ListNode(head.val)
-                p3.next = a
-                p3 = p3.next
-            elif(condition == False and head.val == x):
-                condition = True
-                a = ListNode(head.val)
-                p3.next = a
-                p3 = p3.next
+                p2.next = a
+                p2 = p2.next
             head = head.next
             
-        before_x_greater = before_x_greater.next
         before_x_smaller = before_x_smaller.next
-        if(before_x_smaller == None) : return startingHead
         fromX = fromX.next
-        if(before_x_greater != None):
-            p2.next = before_x_greater
-            p1.next = fromX
-        else:
-             p2.next = fromX
-        
-        
+        if(before_x_smaller == None) : return fromX
+        p1.next = fromX
         return before_x_smaller
-        
         
 
             

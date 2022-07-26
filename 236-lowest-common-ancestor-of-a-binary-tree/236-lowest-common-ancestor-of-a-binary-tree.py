0afc4dfd7f5b18998e13b3e1e9dfb753d9ceda57
Hazem Hany <hazemhany09@gmail.com>
2022-07-26 13:52:53 +0200

    Time: 69 ms (96.30%), Space: 26.3 MB (61.82%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -9,12 +9,16 @@ class TreeNode:
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        if(root == p or root == q or root == None):
-            return root
-        left = self.lowestCommonAncestor(root.left , p , q)
-        right = self.lowestCommonAncestor(root.right , p , q)
+        # if the root is the node we are trying to find (q or p) then return it 
+        # if the root is None that means this branch doesn't have p or q and therfore we return None 
+        if(root == p or root == q or root == None): return root
+        left = self.lowestCommonAncestor(root.left , p , q) # traverse to find q or p
+        right = self.lowestCommonAncestor(root.right , p , q) # traverse to find q or p 
         
-        if(left and right):
-            return root 
-        return left if left else right 
+        if(left and right): return root # if left branch of this node found a node as well as the right brancg 
+                                        # then that means p and q is in separate branches from the this root node 
+                                        # return this root node as the LCA
+                
+        return left if left else right  # if one of the branches is empty that means one of them is the parent of the other
+                                        # return the not None Node as the LCA
                 

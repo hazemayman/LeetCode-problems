d6eb06da430a5e2ee1923b5af3fa1c9dc888baff
Hazem Hany <hazemhany09@gmail.com>
2022-07-27 11:41:19 +0200

    Time: 45 ms (80.41%), Space: 15.3 MB (47.67%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -7,19 +7,28 @@
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
         if(root == None or (root.left == None and root.right == None)):
-            return [root,root]
-        leftBranchNodes = self.flatten(root.left)
-        rightBranchNodes = self.flatten(root.right)
-        if(leftBranchNodes[0] != None):
-            root.right = leftBranchNodes[0]
-            leftBranchNodes[1].right = rightBranchNodes[0]
+            # if the root is None , or doesn't have a righ and child nodes return 
+            # it as the first and last node in that branch 
+            return [root,root] 
+        
+        leftBranchNodes = self.flatten(root.left) # flat the left branch of the root
+        rightBranchNodes = self.flatten(root.right) # flat the right branch of the root
+        if(leftBranchNodes[0] != None): # if the left branch existed 
+            root.right = leftBranchNodes[0] # make the right child of the root is the first Node in the flattened left branch
+            leftBranchNodes[1].right = rightBranchNodes[0] # make the right of the last Node in the flattened left branch 
+                                                           # is the first Node in the flattened right branch 
         else:
-            root.right = rightBranchNodes[0]
-        root.left = None
+            root.right = rightBranchNodes[0] # if the root doesn't have a left branch , make the right child points to the first
+                                             # Node in the flattened right branch
+        root.left = None 
         
 #         print(root.val," - " , rightBranchNodes[1])
 #         print()
-        
-        return [root ,rightBranchNodes[1] if rightBranchNodes[1] else leftBranchNodes[1]]
+    
+        # return the root - first Node in the flattened branch - and the last Node of the flattened branch 
+        # which is equal to the last Node in the right branch if existed otherwise will be the last Node
+        # in the left branch 
+        return [root ,rightBranchNodes[1] if rightBranchNodes[1] else leftBranchNodes[1]] 
+    
         
         
